00b7ffb4a662ffe58d80341d0d3809e65ef8fe8f
4Foshen <120469505+4Foshen@users.noreply.github.com>
2024-10-25 17:36:47 +0500

    init

---
+++ bookshelf_app/lib/system/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  final Color primaryColor = Color(0xFFFED400);
+  final Color secondaryColor = Color(0xFF1500FB);
+  final Color backgroundColor = Colors.white;
+  final Color textColor = Colors.black;
+
+}
